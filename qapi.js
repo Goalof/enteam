17b20672e94825bfa668eee3a99a3d6bd9ac2939
Goalof <68434460+Goalof@users.noreply.github.com>
2021-02-15 19:26:48 +0600

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,192 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"602a760f8db767001e878974",
+				"602a760f8db767001e878976",
+				"602a760f8db767001e878978",
+				"602a760f8db767001e87897a",
+				"602a760f8db767001e87897c",
+				"602a760f8db767001e87897e"
+			]
+		},
+		"602a760f8db767001e878974": {
+			"id": "602a760f8db767001e878974",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"602a760f8db767001e878976": {
+			"id": "602a760f8db767001e878976",
+			"name": "index",
+			"pageUrl": "index",
+			"seo": {
+				"title": "Главная - Работа в uTeam",
+				"description": "Работа в uCoz. Актуальные вакансии компании в российских офисах и удаленная работа. Как попасть в команду, и о нашем коллективе — uTeam."
+			}
+		},
+		"602a760f8db767001e878978": {
+			"id": "602a760f8db767001e878978",
+			"pageUrl": "vacancies",
+			"name": "Вакансии",
+			"seo": {
+				"description": "Вакансии в uCoz. Все вакансии компании по направлениям и проектам. В офисах в Москве, Санкт-Петербурге, Ростове-на-Дону. Удаленная работа.",
+				"title": "Вакансии - Работа в uTeam"
+			},
+			"children": [
+				"602a760f8db767001e878980",
+				"602a760f8db767001e878982",
+				"602a760f8db767001e878984",
+				"602a760f8db767001e878986",
+				"602a760f8db767001e878988",
+				"602a760f8db767001e87898a",
+				"602a760f8db767001e87898c",
+				"602a760f8db767001e87898e",
+				"602a760f8db767001e878990",
+				"602a760f8db767001e878992",
+				"602a760f8db767001e878994",
+				"602a760f8db767001e878996",
+				"602a760f8db767001e878998"
+			]
+		},
+		"602a760f8db767001e87897a": {
+			"id": "602a760f8db767001e87897a",
+			"pageUrl": "uteam",
+			"name": "Команда",
+			"seo": {
+				"description": "uTeam – команда, которая создаёт наши сервисы: uKit, uLanding, uCoz, uCalc. Гордимся не только сервисами, которые создаём, но и тем, что умеем превращать команды профессионалов в настоящих друзей.",
+				"title": "Команда - Работа в uTeam"
+			}
+		},
+		"602a760f8db767001e87897c": {
+			"id": "602a760f8db767001e87897c",
+			"pageUrl": "resume",
+			"name": "Как попасть в команду",
+			"seo": {
+				"description": "Вакансии в uCoz. Все вакансии компании по направлениям и проектам. В офисах в Москве, Санкт-Петербурге, Ростове-на-Дону. Удаленная работа.",
+				"title": "Как попасть в команду - Работа в uTeam"
+			}
+		},
+		"602a760f8db767001e87897e": {
+			"id": "602a760f8db767001e87897e",
+			"pageUrl": "contacts",
+			"name": "Контакты",
+			"seo": {
+				"title": "Контакт - Работа в uTeam",
+				"description": "Вакансии в uCoz. Все вакансии компании по направлениям и проектам. В офисах в Москве, Санкт-Петербурге, Ростове-на-Дону. Удаленная работа."
+			}
+		},
+		"602a760f8db767001e878980": {
+			"id": "602a760f8db767001e878980",
+			"pageUrl": "pr-menedzher",
+			"name": "PR-менеджер",
+			"seo": {
+				"title": "PR-менеджер"
+			}
+		},
+		"602a760f8db767001e878982": {
+			"id": "602a760f8db767001e878982",
+			"pageUrl": "back-end-razrabotchik-php",
+			"name": "Back-end разработчик (PHP)",
+			"seo": {
+				"title": "Работа в uCoz – Back-end разработчик (PHP)"
+			}
+		},
+		"602a760f8db767001e878984": {
+			"id": "602a760f8db767001e878984",
+			"pageUrl": "specialist-po-seo-optimizacii-sajtov",
+			"name": "Специалист по SEO-оптимизации сайтов",
+			"seo": {
+				"title": "Специалист по SEO-оптимизации сайтов"
+			}
+		},
+		"602a760f8db767001e878986": {
+			"id": "602a760f8db767001e878986",
+			"pageUrl": "sborka-sajtov-na-konstruktore-sajtov-ukit",
+			"name": "Сборка сайтов на конструкторе сайтов uKit",
+			"seo": {
+				"title": "Сборка сайтов на конструкторе сайтов uKit"
+			}
+		},
+		"602a760f8db767001e878988": {
+			"id": "602a760f8db767001e878988",
+			"pageUrl": "sborshik-sajtov-na-konstruktorah-ucozuweb",
+			"name": "Сборщик сайтов на конструкторах uCoz/uWeb",
+			"seo": {
+				"title": "Сборщик сайтов на конструкторах uCoz/uWeb"
+			}
+		},
+		"602a760f8db767001e87898a": {
+			"id": "602a760f8db767001e87898a",
+			"pageUrl": "junior-nodejs-razrabotchik",
+			"name": "Junior node.js разработчик",
+			"seo": {
+				"title": "Junior node.js разработчик"
+			}
+		},
+		"602a760f8db767001e87898c": {
+			"id": "602a760f8db767001e87898c",
+			"pageUrl": "menedzher-dizajner-shablonov",
+			"name": "Менеджер-дизайнер шаблонов",
+			"seo": {
+				"title": "Менеджер-дизайнер шаблонов"
+			}
+		},
+		"602a760f8db767001e87898e": {
+			"id": "602a760f8db767001e87898e",
+			"pageUrl": "specialist-sluzhby-tehnicheskoj-podderzhki",
+			"name": "Специалист службы технической поддержки",
+			"seo": {
+				"title": "Специалист службы технической поддержки"
+			}
+		},
+		"602a760f8db767001e878990": {
+			"id": "602a760f8db767001e878990",
+			"pageUrl": "senior-nodejs-engineer",
+			"name": "Senior Node.js engineer",
+			"seo": {
+				"title": "Senior Node.js engineer"
+			}
+		},
+		"602a760f8db767001e878992": {
+			"id": "602a760f8db767001e878992",
+			"pageUrl": "programmist-perl",
+			"name": "Программист Perl",
+			"seo": {
+				"title": "Программист Perl"
+			}
+		},
+		"602a760f8db767001e878994": {
+			"id": "602a760f8db767001e878994",
+			"pageUrl": "specialist-sluzhby-tehnicheskoj-podderzhki1",
+			"name": "Специалист службы технической поддержки",
+			"seo": {
+				"title": "Специалист службы технической поддержки"
+			}
+		},
+		"602a760f8db767001e878996": {
+			"id": "602a760f8db767001e878996",
+			"pageUrl": "pr-menedzher1",
+			"name": "PR-менеджер",
+			"seo": {
+				"title": "PR-менеджер"
+			}
+		},
+		"602a760f8db767001e878998": {
+			"id": "602a760f8db767001e878998",
+			"pageUrl": "technology-evangelistdeveloper-advocate",
+			"name": "Technology Evangelist/Developer Advocate",
+			"seo": {
+				"title": "Technology Evangelist/Developer Advocate"
+			}
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
